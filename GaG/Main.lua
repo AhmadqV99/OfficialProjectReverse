e64ec9f0314f6e41c5c2c6498d868a0267eff8f7
AhmadV99 <juniolancee@gmail.com>
2025-08-09 20:17:03 +0800

    Update Main.lua

--- GaG/Main.lua
+++ GaG/Main.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/4w6wLBwiyjiQmqm8/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadqV99/Permuta/refs/heads/main/lumeniax"))()
